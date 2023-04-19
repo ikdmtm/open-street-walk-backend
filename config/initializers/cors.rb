81eac933ac0642527cadb9a18e16e2b60d20c310
ikdmtm <ikdmtm01@gmail.com>
2023-04-19 21:16:51 +0900

    🐛 corsの設定を修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://open-street-walk.vercel.app", "localhost:4000"
+    origins "https://open-street-walk.vercel.app", "localhost:3000"
 
     resource "*",
       headers: :any,
