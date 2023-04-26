b164290de907963dd26ddc0b1f619a578518c604
ikdmtm <ikdmtm01@gmail.com>
2023-04-26 16:18:38 +0900

    🐛 スペースを削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_10_054024) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_054024) do
     t.string "title", null: false
     t.float "lat", null: false
     t.float "lng", null: false
-    t.bigint "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_pins_on_user_id"
