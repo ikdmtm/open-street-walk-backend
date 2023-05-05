a60ef73621aa18c4d7a52d5eb0f626641d684b2d
ikdmtm <ikdmtm01@gmail.com>
2023-05-06 03:24:53 +0900

    ✨ 画像に対するバリデーションを追加

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,27 @@
 class Pin < ApplicationRecord
     belongs_to :user
+    has_one_attached :image
     validates :lat, presence: true, uniqueness: { scope: :lng }
     validates :lng, presence: true
     validates :title, presence: true, length: { maximum: 25 }
     validates :image, presence: true
-    has_one_attached :image
+    validate :image_type, :image_size
+
+    def image_type
+        if image.blob
+            if !image.blob.content_type.in?(%('image/jpeg image/jpg image/png'))
+            image.purge
+            errors.add(:image, 'はjpegまたはpng形式でアップロードしてください')
+            end
+        end
+    end
     
+    def image_size
+        if image.blob
+            if image.blob.byte_size > 5.megabytes
+            image.purge
+            errors.add(:image, "は1つのファイル5MB以内にしてください")
+            end
+        end
+    end
 end
